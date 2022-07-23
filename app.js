256cb64cc114a039ff16f5253d69daa69aa4b957
MyLadySnezhka <101964822+MyLadySnezhka@users.noreply.github.com>
2022-07-23 14:11:14 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 //console.log('mod is run');
-const axios = require('axios').default;
+const axios = require('axios');
 
 const fs = require('fs/promises');
+const timelog = Date.now();
 
+//const init = async () => {
+   // if(__dirname!='reslog'){
+     // await fs.mkdir(`${__dirname}/reslog`);
+    // else console.log('uzhe est');  
+//}
+
+//init();
+
+let zapislog;
 
 const init = async () => {
-    await fs.mkdir(`${__dirname}/reslog`);
-    result = await axios.get('https://swapi.dev/api/people/1/');
-    console.log(result.data);
+   result = await axios.get('https://swapi.dev/api/people/1/');
+   zapislog = result.data;
 }
 
-init();
+const fileHandler = async () => {
+    await init();   
+    // fs.open('testFile.txt', 'a+');
+    //fs.appendFile(`${timelog}.log`, `\n${zapislog.name}`, 'utf8');
+    fs.writeFile(`${timelog}.log`, `\n${zapislog.name}`, 'utf8');
+    console.log('Done!');
+}
+
+fileHandler();
+
